8c9de2a58e258d20aae575de4779dc76f3ef841f
olivakar <olkar@microsoft.com>
2019-05-30 18:16:59 -0700

    provisioning tests + sample

---
+++ azure-iot-device/samples/advanced-hub-scenarios/register_symmetric_key.py
@@ -0,0 +1,53 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+
+# This is for illustration purposes only. The sample will not work currently.
+
+import os
+import logging
+import asyncio
+from azure.iot.device import SymmetricKeySecurityClient
+from azure.iot.device.aio import SymmetricKeyProvisioningDeviceClient
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+provisioning_host = os.getenv("PROVISIONING_HOST")
+id_scope = os.getenv("PROVISIONING_IDSCOPE")
+registration_id = os.getenv("PROVISIONING_REGISTRATION_ID")
+symmetric_key = os.getenv("PROVISIONING_SYMMETRIC_KEY")
+
+
+async def register_device():
+    symmetric_key_security_client = SymmetricKeySecurityClient(
+        provisioning_host, registration_id, symmetric_key, id_scope
+    )
+    provisioning_device_client = SymmetricKeyProvisioningDeviceClient.create_from_security_client(
+        symmetric_key_security_client, "mqtt"
+    )
+
+    await provisioning_device_client.register()
+
+
+async def main():
+    task1 = asyncio.create_task(register_device())
+    await task1
+
+    # If using Python 3.6 or below, use the following code instead of asyncio.run(main()):
+    # await register_device()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    # If using Python 3.6 or below, use the following code instead of asyncio.run(main()):
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
+
+# Output looks like
+# INFO:azure.iot.device.provisioning.sk_provisioning_device_client:Successfully registered with Hub
